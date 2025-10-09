7391399995465926f3b8d7d3fda5ab6191d504d1
bytemare <3641580+bytemare@users.noreply.github.com>
2025-10-09 09:57:57 +0200

    debug
    Signed-off-by: bytemare <3641580+bytemare@users.noreply.github.com>

--- scripts/package-source.sh
+++ scripts/package-source.sh
@@ -70,7 +70,7 @@ if [ "${GITHUB_REF_TYPE:-}" = "tag" ]; then
 else
   TAG_SAFE="$(sanitize "${GITHUB_REF_NAME//\//_}")-dryrun-${GITHUB_RUN_NUMBER}"
 fi
-BASENAME=$(echo "${REPO_SAFE}-${TAG_SAFE}" | tr -d '\r')
+BASENAME=$(echo "${REPO_SAFE}-${TAG_SAFE}" | tr -dc '[:print:]')
 OUTDIR=dist
 mkdir -p "$OUTDIR"
 ARCHIVE_PATH="${OUTDIR}/${BASENAME}.tar.gz"
